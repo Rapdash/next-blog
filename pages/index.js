4b91a31ea416a7cf120bfac295d13c82406bfece
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 10:25:01 -0800

    shifted order of card & column

--- pages/index.js
+++ pages/index.js
@@ -7,22 +7,23 @@ const IndexPage = () => (
     {/* Margin is for the navbar */}
     <Row>
       <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} className="mt-4">
-        <Row>
-          <Col>
-            <Link href={"/articles/1"} passHref>
-            <a className="no-link-hover">
-              <Card>
-                <Card.Header className="bg-black">
-                  <Card.Title className="text-white">
-                    Start Here: Why You Need This Blog
-                  </Card.Title>
-                  <Card.Subtitle className="text-green">Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
-                </Card.Header>
-              </Card>
-            </a>
-            </Link>
+        <Link href={"/articles/1"} passHref>
+        <a className="no-link-hover">
+        <Card>
+          <Row>
+            <Col xs={{ span: 12 }} lg={{ span: 6 }}>
+              <Card.Header className="bg-black">
+                <Card.Title className="text-white">
+                  Start Here: Why You Need This Blog
+                </Card.Title>
+                <Card.Subtitle className="text-green">Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
+              </Card.Header>
           </Col>
         </Row>
+
+        </Card>
+            </a>
+            </Link>
       </Col>
     </Row>
   </Layout>
