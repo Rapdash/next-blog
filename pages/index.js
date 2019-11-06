04f502c55fc5bb77af7b6b15a3e0860e521dd8de
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 10:18:23 -0800

    added some css utils to global css

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import { Layout } from "../components";
 import { Row, Col, Card } from "react-bootstrap";
+import Link from "next/link"
 
 const IndexPage = () => (
   <Layout>
     {/* Margin is for the navbar */}
     <Row>
-      <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} className="mt-4" >
-        <Card>
-          <Card.Header style={{ backgroundColor: 'black' }}>
-            <Card.Title>
-              Start Here: Why You Need This Blog
-            </Card.Title>
-            <Card.Subtitle>Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
-          </Card.Header>
-        </Card>
+      <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} className="mt-4">
+        <Link href={"/articles/1"} passHref>
+        <a>
+          <Card>
+            <Card.Header className="bg-black">
+              <Card.Title className="text-white">
+                Start Here: Why You Need This Blog
+              </Card.Title>
+              <Card.Subtitle className="text-green">Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
+            </Card.Header>
+          </Card>
+        </a>
+        </Link>
       </Col>
     </Row>
   </Layout>
