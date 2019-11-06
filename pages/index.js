bff3c647623e2dc5f4927f1a0404ce28fe6b8fe4
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 10:27:30 -0800

    changing css

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,20 @@ const IndexPage = () => (
       <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} className="mt-4">
         <Link href={"/articles/1"} passHref>
         <a className="no-link-hover">
-        <Card>
+        <Card className="bg-black">
           <Row>
             <Col xs={{ span: 12 }} lg={{ span: 6 }}>
-              <Card.Header className="bg-black">
+              <Card.Header>
                 <Card.Title className="text-white">
                   Start Here: Why You Need This Blog
                 </Card.Title>
                 <Card.Subtitle className="text-green">Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
               </Card.Header>
-          </Col>
-        </Row>
-
+            </Col>
+          </Row>
         </Card>
-            </a>
-            </Link>
+        </a>
+        </Link>
       </Col>
     </Row>
   </Layout>
