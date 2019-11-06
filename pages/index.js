7f23dd26dce8995e9226210c73a5fa34dc13ecbd
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 10:10:17 -0800

    background color

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import { Layout } from "../components";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 
 const IndexPage = () => (
   <Layout>
     {/* Margin is for the navbar */}
     <Row>
-      <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} style={{ backgroundColor: "gray" }}>
-        
+      <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} className="mt-4" >
+        <Card>
+          <Card.Header style={{ backgroundColor: 'black' }}>
+            <Card.Title>
+              Start Here: Why You Need This Blog
+            </Card.Title>
+            <Card.Subtitle>Published Nov. 6, 2019 - 0 Comments</Card.Subtitle>
+          </Card.Header>
+        </Card>
       </Col>
     </Row>
   </Layout>
