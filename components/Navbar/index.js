dd95e788f19bddb08fb336a82f8bba3c37da4fef
Frederick Johnson <fritzj96@gmail.com>
2019-11-05 21:09:51 -0800

    installed & added LinkContainer from react-router-bootstrap

---
+++ components/Navbar/index.js
@@ -0,0 +1,12 @@
+import { Navbar as BSNavbar, Nav} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Navbar = () => (
+  <BSNavbar>
+    <LinkContainer>
+      <BSNavbar.Brand>
+        test
+      </BSNavbar.Brand>>
+    </LinkContainer>
+  </BSNavbar>
+)
