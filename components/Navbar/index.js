1b36c2883c573c061346742f1e7e47d9376aa683
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 09:17:26 -0800

    added correct paths

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,4 +1,4 @@
-import { Navbar as BSNavbar, Nav} from "react-bootstrap";
+import { Navbar as BSNavbar, Nav, Button } from "react-bootstrap";
 import Link from "next/link"
 
 import "./navbar.css";
@@ -6,7 +6,7 @@ import "./navbar.css";
 export const Navbar = () => (
   <BSNavbar expand="md" variant="dark">
 
-    <Link href="/">
+    <Link href="/" passHref>
       <BSNavbar.Brand>
         <span style={{ color: 'white'}}>Hack</span>Aspergers
       </BSNavbar.Brand>
@@ -15,7 +15,7 @@ export const Navbar = () => (
     <BSNavbar.Collapse>
       <Nav>
         <Nav.Item>
-          <Link passHref href="/latest-post">
+          <Link passHref href="/">
             <Nav.Link active>Home</Nav.Link>
           </Link>
         </Nav.Item>
@@ -25,13 +25,20 @@ export const Navbar = () => (
           </Link>
         </Nav.Item>
         <Nav.Item>
-          <Link passHref href="/latest-post">
+          <Link passHref href="/archives">
             <Nav.Link active>Archives</Nav.Link>
           </Link>
         </Nav.Item>
         <Nav.Item>
-          <Link passHref href="/latest-post">
+          <Link passHref href="/about">
             <Nav.Link active>About The Author</Nav.Link>
+          </Link>
+        </Nav.Item>
+      </Nav>
+      <Nav className="ml-auto">
+        <Nav.Item>
+          <Link passHref href="">
+            <Button style={{ backgroundColor: "rgb(0, 255, 0)", color: "black" }}>Contact About Coaching</Button>
           </Link>
         </Nav.Item>
       </Nav>
