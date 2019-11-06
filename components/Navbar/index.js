738e28d838ab29996163e91c63894def4710f861
Frederick Johnson <fritzj96@gmail.com>
2019-11-05 21:10:22 -0800

    exports

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,7 +1,7 @@
 import { Navbar as BSNavbar, Nav} from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const Navbar = () => (
+export const Navbar = () => (
   <BSNavbar>
     <LinkContainer>
       <BSNavbar.Brand>
