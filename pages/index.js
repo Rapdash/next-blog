cbb2e765c6b9c3d6b57d3c3f3f5faec76cb6a409
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 09:55:43 -0800

    Adding stuff to index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import { Layout } from "../components";
+import { Row, Col } from "react-bootstrap";
 
 export default () => (
   <Layout>
-    
+    <Row style={{ marginTop: 65 }}>
+      <Col xs={{ span: 12 }} sm={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 1 }} style={{ backgroundColor: "gray" }}>
+        
+      </Col>
+    </Row>
   </Layout>
 )
