d0a64cfe245d8add00810c89a4f54ceb0a38b569
Frederick Johnson <fritzj96@gmail.com>
2019-11-06 09:08:54 -0800

    built the navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,12 +4,37 @@ import Link from "next/link"
 import "./navbar.css";
 
 export const Navbar = () => (
-  <BSNavbar variant="dark">
+  <BSNavbar expand="md" variant="dark">
 
     <Link href="/">
       <BSNavbar.Brand>
         <span style={{ color: 'white'}}>Hack</span>Aspergers
       </BSNavbar.Brand>
     </Link>
+    <BSNavbar.Toggle />
+    <BSNavbar.Collapse>
+      <Nav>
+        <Nav.Item>
+          <Link passHref href="/latest-post">
+            <Nav.Link active>Home</Nav.Link>
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link passHref href="/latest-post">
+            <Nav.Link active>Latest Post</Nav.Link>
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link passHref href="/latest-post">
+            <Nav.Link active>Archives</Nav.Link>
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link passHref href="/latest-post">
+            <Nav.Link active>About The Author</Nav.Link>
+          </Link>
+        </Nav.Item>
+      </Nav>
+    </BSNavbar.Collapse>
   </BSNavbar>
 )
